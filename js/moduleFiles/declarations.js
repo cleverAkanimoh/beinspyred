3d56989db8b84508fbe1733ba8c3627cdb9f55a7
Clever AKANIMOH <crushclever1@gmail.com>
2022-10-30 04:50:52 +0100

    updated declarations, app.min working

--- js/moduleFiles/declarations.js
+++ js/moduleFiles/declarations.js
@@ -1,14 +1,7 @@
 const welcome = document.querySelector('#welcome');
-
 const main = document.querySelector('main');
 
 const navbar = document.querySelector('.nav');
-
-// const toggleBtn = document.querySelector('.toggle-btn');
-// const sidebarWrapper = document.querySelector('.sidebar-wrapper');
-// const sidebar = document.querySelector('.sidebar-links');
-// const linkBtns = [...document.querySelectorAll('.link-btn')];
-// const submenu = document.querySelector('.submenu');
 
 const mainBody = document.querySelector('#mainBody');
 const hero = document.querySelector('.hero');
@@ -23,16 +16,7 @@ const date = document.querySelector('#date');
 
 const items = [...document.querySelectorAll('.number')];
 
-
 const imagesEnvelop = document.querySelector('.imagesEnvelop');
 const galleryWrapper = document.querySelector('.galleryWrapper');
-
-// const continueBtn = document.querySelector('#continueBtn');
-// const formInputs = document.querySelector('.formInputs');
-// const wordCount = document.querySelector('form label');
-// const errorText = document.querySelector('form legend');
-// const textArea = document.querySelector('form textarea');
-// const userEmail = document.querySelector('#userEmail');
-// const submit = document.querySelector('#submitInput');
 
 export { welcome, main, navbar, mainBody, hero, beinspyredText, logos, contactBtn, contactWrapper, date, items, imagesEnvelop, galleryWrapper };
